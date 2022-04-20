f645560e32c98cdd0bb3053065f394c2fc96616f
Luis Canales <lacanales@ucdavis.edu>
2022-04-19 20:13:10 -0700

    added sample data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,18 @@
 class PagesController < ApplicationController
+include PagesHelper
     def index
         if !user_signed_in?
             redirect_to welcome_path
+            return
         end
+        # @token = session[:access_token]
+        #client = Strava::Api::Client.new(access_token: session[:access_token])
+        #@activities = client.athlete_activities
+        
+        print sample_data
+        @activities = sample_data
     end
-    def welcome
 
+    def welcome
     end
 end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def strava
         @user = User.from_omniauth(request.env["omniauth.auth"])
+        session[:access_token] = request.env["omniauth.auth"].credentials.token
         if @user.persisted?
             sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
             set_flash_message(:notice, :success, kind: "strava") if is_navigational_format?
