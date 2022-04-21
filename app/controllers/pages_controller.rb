0e22c668f7b5284651be356d75d458ab59b24543
Luis Canales <cluchomucho@gmail.com>
2022-04-20 21:32:25 -0700

    cleaned up some chart options

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,13 +13,13 @@ include PagesHelper
       after  = params[:dates][:from].empty? ? Time.now - 30.days : Time.parse(params[:dates][:from])
       before = params[:dates][:to].empty?   ? Time.now : Time.parse(params[:dates][:to])
       @activities = get_all_activities(after,before)
-      
+
       ## TODO: Put in a routine that actuall refreshes your token.
       if @activities.nil?
-        redirect_to destroy_user_session_path 
+        redirect_to destroy_user_session_path
         return
       end
-      
+
       @week_mileage = weekly_counter(@activities,after)
     end
 
@@ -34,14 +34,11 @@ include PagesHelper
       sum = 0.0
       activities.reverse_each.with_index do |activity,index|
         if activity.start_date <= next_week
-          print("Normal sum")
           sum += activity.distance_in_miles
           if index == activities.length-1
-            print("hit end of array")
             week_mileage[key_week.to_date] = sum
           end
         else
-          print("Hit end of a week sum")
           week_mileage[key_week.to_date] = sum
           sum = 0.0
           key_week = next_week
